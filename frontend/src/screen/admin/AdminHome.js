37bc85ea75df90abe1dfa2630be62d3638953ff5
Vishwanath <vishwanathvishwabai@gmail.com>
2021-12-03 10:06:30 +0530

    final commit

--- frontend/src/screen/admin/AdminHome.js
+++ frontend/src/screen/admin/AdminHome.js
@@ -8,14 +8,24 @@ const AdminHome = () => {
             <Navbar />
             <div className="admin__home">
                 <div className="admin__home_box">
-                    <Link to="/studentDetails" className="admin__home_student">
+                    <Link to="/admin/studentDetails" className="admin__home_student">
                         <div className="admin__student_icon"></div>
                         <div className="">
                             students
                         </div>
                     </Link> 
-                    <div className="admin__home_nomination"></div>
-                    <div className="admin__home_voting"></div>
+                    <Link to="/admin/nominationDetails" className="admin__home_nomination">
+                        <div className="admin__student_icon"></div>
+                        <div className="">
+                            nomination
+                        </div>
+                    </Link> 
+                    <Link to="/admin/votingDetails" className="admin__home_voting">
+                        <div className="admin__student_icon"></div>
+                        <div className="">
+                            vote
+                        </div>
+                    </Link> 
                 </div>
             </div>
         </>
